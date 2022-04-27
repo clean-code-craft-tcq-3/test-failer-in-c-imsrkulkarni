144f3e37ec403ebda97ccd5c5f6e1f111f9ff10d
imsrkulkarni <77853418+imsrkulkarni@users.noreply.github.com>
2022-04-27 19:34:52 +0530

    Update tshirts.c

--- tshirts.c
+++ tshirts.c
@@ -14,7 +14,7 @@ char size(int cms) {
 }
 
 int main() {
-    assert(size(38) == 'S');
+    assert(size(37) == 'S');
     assert(size(38) == 'M');
     assert(size(42) == 'L');
     printf("All is well (maybe!)\n");
